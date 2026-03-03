8d40e8babc282fe1ebeb182ba7440a21c2e7a567
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-03 17:25:52 +0530

    updated

--- Java-vs/buffered-codes/circle.java
+++ Java-vs/buffered-codes/circle.java
@@ -1,6 +1,16 @@
-import java.util.*;
-class circle
+import java.io.*;
+class circle 
 {
-    public static void main(String args[])
-    
+    public static void main(String[] args) throws IOException 
+    {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.print("Enter the radius of the circle: ");
+        double radius =Double.parseDouble(br.readLine());
+        
+        double area = Math.PI * radius * radius;
+        
+        System.out.println("Area of the circle is: " + area);
+    }
 }
